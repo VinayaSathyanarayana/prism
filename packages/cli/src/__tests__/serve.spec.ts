57cb7e80fe128dbaffa45cc637b23b7b7376d343
Karol Maciaszek <karol@maciaszek.pl>
2018-11-15 17:23:01 +0100

    SL-469 Non-working test

--- packages/cli/src/__tests__/serve.spec.ts
+++ packages/cli/src/__tests__/serve.spec.ts
@@ -1,7 +1,40 @@
-import Serve from '../commands/serve';
+import axios from 'axios';
+const { execFile } = require('child_process');
 
 describe('serve', () => {
-  test.skip('testName', () => {
-    return Serve.run();
+  let serve: any;
+
+  beforeAll(() => {
+    serve = execFile('node', ['./packages/cli/bin/run', 'serve']);
+
+    return new Promise((resolve, reject) => {
+      serve.stdout.on('data', (message: string) => {
+        console.log(`stdout: ${message}`);
+        resolve();
+      });
+
+      serve.stderr.on('data', (message: string) => {
+        console.log(`stderr: ${message}`);
+        reject();
+      });
+    });
+  });
+
+  it('testName', async () => {
+    const response = await axios({
+      url: 'http://localhost:4010/',
+    });
+
+    console.log(response);
+
+    expect(true).toBe(true);
+  });
+
+  afterAll(() => {
+    serve.unref();
+
+    serve.kill();
+
+    return new Promise(resolve => serve.on('exit', resolve));
   });
 });
